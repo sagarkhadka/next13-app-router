263bdf559250d06edd2c9f91f135a0f3cab37753
Sagar Khadka <khadkasagar109@gmail.com>
2023-06-28 23:09:34 +0545

    next/font setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,17 @@
 import '@styles/global.css'
+import { Manrope, Sora } from '@next/font/google'
+
+const manrope = Manrope({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--manrope'
+})
+
+const sora = Sora({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--sora'
+})
 
 export const metadata = {
   title: 'Promtopia',
@@ -11,8 +24,20 @@ interface RootLayoutProps {
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={`${manrope.variable} ${sora.variable} font-sans`}>
+      {/* <style
+        jsx
+        global>
+        html{' '}
+        {`
+          --manrope: ${manrope.style.fontFamily};
+          --sora: ${sora.style.fontFamily};
+        `}
+      </style> */}
+
+      <body className='font-manrope'>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
       }
     },
     extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        sora: ['Sora', 'sans-serif']
+      },
       keyframes: {
         'accordion-down': {
           from: { height: 0 },
